18c3bc5828c68fab141b90745bcebd6e53b366da
alexzhang1030 <1642114555@qq.com>
2023-05-17 10:31:37 +0800

    chore: remove unused

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,6 @@
 import type { ZodObject, ZodOptional } from 'zod'
 import { ZodFirstPartyTypeKind } from 'zod'
 
-export function instanceOf(value: any, constructor: Function) {
-  return value instanceof constructor
-}
-
 export const zodIs = {
   object(type: ZodFirstPartyTypeKind, value: any): value is ZodObject<any> {
     return type === ZodFirstPartyTypeKind.ZodObject
