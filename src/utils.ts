85ef783116caf9a3dab1a8ab90374d39a13c7450
alexzhang1030 <1642114555@qq.com>
2023-05-18 19:35:52 +0800

    fix: type

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,11 @@
-import type { ZodObject, ZodOptional } from 'zod'
+import type { ZodObject, ZodOptional, ZodTypeAny } from 'zod'
 import { ZodFirstPartyTypeKind } from 'zod'
 
 export const zodIs = {
-  object(type: ZodFirstPartyTypeKind, value: any): value is ZodObject<any> {
+  object(type: ZodFirstPartyTypeKind, value: ZodTypeAny): value is ZodObject<any> {
     return type === ZodFirstPartyTypeKind.ZodObject
   },
-  optional(type: ZodFirstPartyTypeKind, value: any): value is ZodOptional<any> {
+  optional(type: ZodFirstPartyTypeKind, value: ZodTypeAny): value is ZodOptional<any> {
     return type === ZodFirstPartyTypeKind.ZodOptional
   },
 }
